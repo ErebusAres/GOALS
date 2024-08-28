13c0fe771a8b4263491f1f40ba34f580a993a651
ErebusAres <erebusares@gmail.com>
2024-08-28 13:30:42 -0500

    Update testing.lua

--- TESTING/testing.lua
+++ TESTING/testing.lua
@@ -1,2 +1,19 @@
 -- wowprogramming.com ; reference page for api
 
+local s = 5;
+local myFunc = function() 
+    local s;
+    s = 2;
+    return s;
+end
+
+s = myFunc();
+print(s) -- returns 2
+
+local function counter(a, b, c)
+    local ans = (a * b) + c;
+
+    return ans;
+end
+
+print(counter(15, 3, 600));
