3c37625f189a3338290a1a1131d541c4bed845a6
ErebusAres <erebusares@gmail.com>
2024-09-05 10:57:38 -0500

    Update testing.lua

--- TESTING/testing.lua
+++ TESTING/testing.lua
@@ -7,10 +7,8 @@ SLASH_FRAMESTK1 = "/fs"
 local function OnEvent(self, event, ...)
     local _, subevent, _, sourceName, _, _, destName, _, prefixParam1, prefixParam2, _, suffixParam1, suffixParam2 = ...
   
-    if (subevent == "SPELL_DAMAGE" or subevent == "SPELL_PERIODIC_DAMAGE" or subevent == "RANGE_DAMAGE") and suffixParam2 > 0 then
-       print("["..sourceName.."] killed ["..destName.."] with "..suffixParam1.." "..GetSpellLink(prefixParam1))
-    elseif subevent == "SWING_DAMAGE" and prefixParam2 > 0 then
-       print("["..sourceName.."] killed ["..destName.."] with "..prefixParam1.." Melee")
+    if subevent == "UNIT_DIED" then
+       print("["..sourceName.."] killed ["..destName.."]")
     end
  end
   
